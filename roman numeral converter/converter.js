41b9dcf3d0a83ef1cbcfce37313445adaf4eadef
Soham Bihari <76133731+Soham1213@users.noreply.github.com>
2022-04-16 15:11:21 -0700

    Create converter.js

---
+++ roman numeral converter/converter.js
@@ -0,0 +1,30 @@
+function convertToRoman(num) {
+ const romanTable = {
+   M: 1000,
+   CM: 900,
+   D: 500,
+   CD: 400,
+   C: 100,
+   XC: 90,
+   L: 50,
+   XL: 40,
+   X: 10,
+   IX: 9,
+   V: 5,
+   IV: 4,
+   I: 1,
+ };
+ 
+ let accumulator = '';
+
+ for (const symbol in romanTable) {
+   const romanEquals = romanTable[symbol];
+   while (romanEquals <= num) {
+     num-= romanEquals;
+     accumulator+=symbol;
+   }
+  }
+  return accumulator;
+}
+
+convertToRoman(36);
