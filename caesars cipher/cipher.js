c55ba110cb685957c326c894ab3fcc563ffc75a8
Soham Bihari <76133731+Soham1213@users.noreply.github.com>
2022-04-16 15:14:07 -0700

    Create cipher.js

---
+++ caesars cipher/cipher.js
@@ -0,0 +1,47 @@
+const letter = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'                                      
+];
+
+
+function rot13(str) {
+  let accumulator = '';
+
+  for (let x = 0; x < str.length; x++) {
+    const char = str[x];
+    const aLetter = letter.includes(char);
+    if (aLetter) {
+      const charValue = letter.findIndex((c) => c === char);
+      accumulator += letter[charValue + 13] || letter[charValue - 13];
+    } else {
+      accumulator+= char;
+    }
+  }
+  return accumulator;
+}
+
+console.log(rot13("SERR PBQR PNZC"));
