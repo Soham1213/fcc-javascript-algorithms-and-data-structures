8837ee7e26584b71fec6ea1971eea0c20f7073bd
Soham Bihari <76133731+Soham1213@users.noreply.github.com>
2022-04-16 15:15:02 -0700

    Update cipher.js

--- caesars cipher/cipher.js
+++ caesars cipher/cipher.js
@@ -45,3 +45,6 @@ function rot13(str) {
 }
 
 console.log(rot13("SERR PBQR PNZC"));
+console.log(rot13("SERR CVMMN!"));
+console.log(rot13("SERR YBIR?"));
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
